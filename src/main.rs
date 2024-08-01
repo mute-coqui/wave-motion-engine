d08dd4ccd1c4e4f05b369856a7c5fe2a929f1732
jeremy <mute.coqui@gmail.com>
2024-07-31 22:04:25 -0500

    Adding gl and glfw for OpenGL and windowing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+extern crate glfw;
+extern crate gl;
+
+use glfw::{Action, Context, Key};
+
 fn main() {
-    println!("Hello, world!");
+    let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
+
+    let (mut window, events) = glfw
+        .create_window(1920, 1080, "Wave Motion Engine", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window");
+
+    window.set_key_polling(true);
+    window.make_current();
+
+    gl::load_with(|symbol| window.get_proc_address(symbol));
+
+    while !window.should_close() {
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            handle_window_events(&mut window, event);
+        }
+
+        unsafe {
+            gl::ClearColor(0.3, 0.3, 0.3, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+        window.swap_buffers();
+    }
+}
+
+fn handle_window_events(window: &mut glfw::Window, event: glfw::WindowEvent) {
+    match event {
+        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+            window.set_should_close(true);
+        }
+        _ => {}
+    }
 }
